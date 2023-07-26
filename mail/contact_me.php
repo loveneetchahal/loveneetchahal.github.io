16d3b0d3a1d9666ae153b18057e6da32c871927c
Loveneet Singh Chahal <chahal.loveneet@gmail.com>
2023-07-26 18:34:06 +0530

    Update contact_me.php
    email changes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'loveneet0040@gmail.com'; 
+$to = 'chahal.loveneet@gmail.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. 
